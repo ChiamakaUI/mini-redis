99f094d25b492b39ce737192587ed474c3dc0d3b
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2026-02-02 16:46:14 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::{error::Error, sync::Arc};
+
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+
+mod store;
+use store::{Command, Storage, Store};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let store = Arc::new(Store::new());
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    println!("Server listening on 127.0.0.1:8080");
+
+    loop {
+        let (socket, addr) = listener.accept().await?;
+
+        let store = Arc::clone(&store);
+
+        println!("New client connected: {:?}", addr);
+        tokio::spawn(async move { handle_client(socket, store).await });
+    }
+}
+
+async fn handle_client(socket: TcpStream, store: Arc<Store<String>>) {
+    let mut reader = BufReader::new(socket);
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        match reader.read_line(&mut line).await {
+            Ok(0) => break,
+            Ok(_) => {
+                let command = Command::from_str(&line);
+                let response = match command {
+                    Command::Get(key) => store.get(&key).unwrap_or_else(|| "(nil)".to_string()),
+                    Command::Set(key, val) => {
+                        store.set(key, val);
+                        "OK".to_string()
+                    }
+                    Command::Del(key) => {
+                        if store.delete(&key) {
+                            "(integer) 1".to_string()
+                        } else {
+                            "(integer) 0".to_string()
+                        }
+                    }
+                    Command::Unknown => "ERR unknown command".to_string(),
+                };
+
+                if let Err(e) = reader
+                    .get_mut()
+                    .write_all(format!("{}\n", response).as_bytes())
+                    .await
+                {
+                    eprintln!("Failed to write to socket: {}", e);
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Read error: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+// Arc<RwLock<HashMap<String, String>>>
+// let store: Arc<RwLock<HashMap<String, String>>> = Arc::new(RwLock::new(HashMap::new()));
---
+++ src/store.rs
@@ -0,0 +1,67 @@
+use std::{collections::HashMap, sync::RwLock};
+pub enum Command {
+    Get(String),
+    Set(String, String),
+    Del(String),
+    Unknown,
+}
+
+impl Command {
+    pub fn from_str(input: &str) -> Self {
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+
+        match parts.as_slice() {
+            ["GET", key] => Command::Get(key.to_string()),
+            ["SET", key, val] => Command::Set(key.to_string(), val.to_string()),
+            ["DEL", key] => Command::Del(key.to_string()),
+            _ => Command::Unknown,
+        }
+    }
+}
+
+pub trait Storage<T> {
+    fn get(&self, key: &str) -> Option<T>;
+    fn set(&self, key: String, val: T);
+    fn delete(&self, key: &str) -> bool;
+}
+
+pub struct Store<T> {
+    inner: RwLock<HashMap<String, T>>,
+}
+
+impl<T> Store<T> {
+    pub fn new() -> Self {
+        Self {
+            inner: RwLock::new(HashMap::new()),
+        }
+    }
+}
+
+impl<T> Storage<T> for Store<T>
+where
+    T: Clone + Send + Sync,
+{
+    fn get(&self, key: &str) -> Option<T> {
+        let val = &self.inner.read().unwrap();
+        val.get(key).cloned()
+    }
+
+    fn set(&self, key: String, val: T) {
+        let mut map = self.inner.write().unwrap();
+        map.insert(key, val);
+    }
+
+    fn delete(&self, key: &str) -> bool {
+        let mut map = self.inner.write().unwrap();
+        map.remove(key).is_some()
+    }
+}
+
+// impl StoreValue {
+//     pub fn run<S>(&self, storage: &S)
+//     where
+//         S: Storage
+//     {
+//         // self.key
+//     }
+// }
